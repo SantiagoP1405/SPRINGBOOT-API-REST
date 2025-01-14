40f7fcf7acc9c64858c7b1147e4422dacf419afe
Santiago Gómez <santipatito04@gmail.com>
2025-01-14 14:10:33 -0600

    Añadir al repositorio
    Se modificó el método del coontrolador para añadir el parámetro recibido al repositorio

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,17 @@
 package com.santiagogomez.springbootapi.med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.santiagogomez.springbootapi.med.voll.api.medico.DatosRegistroMedico;
+import com.santiagogomez.springbootapi.med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico) {
