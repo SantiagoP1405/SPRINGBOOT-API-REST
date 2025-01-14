a1d33ea20621c52474ce2f25eece5ec6780cfaa0
Santiago Gómez <santipatito04@gmail.com>
2025-01-14 14:13:27 -0600

    Constructor Direccion
    Se creó un nuevo constructor para Direccion que reciba DatosDireccion

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/direccion/Direccion.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/direccion/Direccion.java
@@ -10,10 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor // Lombok genera un constructor vacío
 @AllArgsConstructor // Lombok genera un constructor con todos los argumentos
 public class Direccion {
+    
     private String calle;
     private String distrito;
     private String ciudad;
     private String numero;
     private String complemento;
-
+    
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+        this.numero = direccion.numero();
+        this.complemento = direccion.complemento();
+    }
 }
