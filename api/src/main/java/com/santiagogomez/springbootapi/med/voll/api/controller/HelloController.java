7d99507849ae76f47f3cdb99c66fcc0051af642e
Santiago Gómez <santipatito04@gmail.com>
2025-01-14 12:30:39 -0600

    Método hello
    Se creó un método con RestController y un mapeo para mostrar un mensaje

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/HelloController.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/HelloController.java
@@ -1,5 +1,14 @@
 package com.santiagogomez.springbootapi.med.voll.api.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/hello") //Decirle qqué ruta de http está siguiendo
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
 }
