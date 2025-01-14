56b202e969f9847bfa0b04a0779ea2fac07729ae
Santiago Gómez <santipatito04@gmail.com>
2025-01-14 16:29:40 -0600

    Añadir validaciones
    Se añadieron distintas validaciones en los datos de médico y de registro para aegurarse de que no lleguen null

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/direccion/DatosDireccion.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/direccion/DatosDireccion.java
@@ -1,10 +1,22 @@
 package com.santiagogomez.springbootapi.med.voll.api.direccion;
 
+import jakarta.validation.constraints.*;
+
 public record DatosDireccion(
+
+    @NotBlank
     String calle,
+    
+    @NotBlank
     String distrito,
+    
+    @NotBlank
     String ciudad,
+    
+    @NotBlank
     String numero,
+    
+    @NotBlank
     String complemento
 ) {
 
--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/medico/DatosRegistroMedico.java
@@ -2,11 +2,25 @@ package com.santiagogomez.springbootapi.med.voll.api.medico;
 
 import com.santiagogomez.springbootapi.med.voll.api.direccion.DatosDireccion;
 
+import jakarta.validation.constraints.*;
+
 public record DatosRegistroMedico(
+    @NotNull 
+    @NotBlank
     String nombre, 
+
+    @NotBlank
+    @Email
     String email,
+
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}")// Para limitar los dígitos del documento
     String documento,
+
+    @NotBlank
     Especialidad especialidad,
+
+    @NotNull
     DatosDireccion direccion
     
     ) {
