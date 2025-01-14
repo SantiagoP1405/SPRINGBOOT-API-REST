694cd40509ca1dc5a6c3b55e1b8d0965ae0ed0d6
Santiago Gómez <santipatito04@gmail.com>
2025-01-14 16:33:05 -0600

    Validation
    Se añade validation al controlador para aplicar las validaciones

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,8 @@ import com.santiagogomez.springbootapi.med.voll.api.medico.DatosRegistroMedico;
 import com.santiagogomez.springbootapi.med.voll.api.medico.Medico;
 import com.santiagogomez.springbootapi.med.voll.api.medico.MedicoRepository;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
@@ -15,7 +17,7 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico) {
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
         medicoRepository.save(new Medico(datosRegistroMedico));
     }   
 }
