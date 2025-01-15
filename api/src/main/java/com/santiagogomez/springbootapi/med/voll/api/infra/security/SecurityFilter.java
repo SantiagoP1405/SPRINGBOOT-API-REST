535f16742356823ea29ed999240a3277ea8237df
Santiago Gómez <santipatito04@gmail.com>
2025-01-15 16:32:15 -0600

    Manejo de excepciones para el token
    Se implementó la validación del token para evitar NullPointerException y RuntimeException. Se asegura que el token no esté vacío o nulo antes de proceder con el reemplazo.

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = request.getHeader("Authorization").replace("Bearer ", "");
+        var token = request.getHeader("Authorization");
+        if (token == "" || token == null) {
+            throw new RuntimeException("Token inválido");
+        }
+        token = token.replace("Bearer ", "");
         System.out.println(token);
         filterChain.doFilter(request, response); //llama al siguiente filtro
     }
