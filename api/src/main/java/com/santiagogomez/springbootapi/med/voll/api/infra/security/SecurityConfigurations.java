686c197048ba1fc160fc6e7d3976309b51d722b6
Santiago Gómez <santipatito04@gmail.com>
2025-01-15 17:03:29 -0600

    Acceso al endpoint de Login
    Modificado SecurityConfigurations para permitir solicitudes POST a /Login
    Añadido PermitAll para el acceso público al Login
    Ajustado el manejo de tokens para evitar errores de validación en el Login

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.santiagogomez.springbootapi.med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +20,13 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf().disable().sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().authorizeRequests()
+        .requestMatchers(HttpMethod.POST, "/login")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and().build();
     }
 
     @Bean
