0e9f6d010ba64a1f65a5f81e6d128d74576faa69
Santiago Gómez <santipatito04@gmail.com>
2025-01-15 17:29:33 -0600

    Implementar control de acceso en la API Rest
    Agregado filtro de seguridad personalizado para manejar la autenticación de usuarios.
    Modificado la configuración de seguridad para que el filtro personalizado se ejecute antes del filtro por defecto de Spring.
    Implementada lógica para forzar el inicio de sesión del usuario basado en el token recibido.
    Refactorizado el código para mantener la limpieza y legibilidad.
    Añadidos casos de prueba para verificar el funcionamiento del login y el acceso a endpoints protegidos.

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/security/SecurityFilter.java
@@ -3,30 +3,42 @@ package com.santiagogomez.springbootapi.med.voll.api.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.santiagogomez.springbootapi.med.voll.api.domain.usuarios.UsuarioRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.experimental.var;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
     
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Inicio del filter");
-        var token = request.getHeader("Authorization");
-        System.out.println(token);
-        if(token != null){
-            System.out.println("Token no es null");
-            token = token.replace("Bearer ", "");
-            System.out.println(token);
-            System.out.println(tokenService.getSubject(token));
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader != null){
+            var token = authHeader.replace("Bearer ", "");
+            var subject = tokenService.getSubject(token);
+            if (subject != null) {
+                //Para este punto el token ya es válidop
+                var usuario = usuarioRepository.findByLogin(subject);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario,
+                null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);;
+            }
         }
         filterChain.doFilter(request, response); //llama al siguiente filtro
     }
