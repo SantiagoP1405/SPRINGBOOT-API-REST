8f1ecd1a0069917c9f2a2d605d7d0150847aa2fd
Santiago Gómez <santipatito04@gmail.com>
2025-01-15 12:52:55 -0600

    Crear Load by username
    Funciónn en autenticación service para buscar usuarios con el username usando el repositorio.

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/usuarios/UsuarioRepository.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/usuarios/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.santiagogomez.springbootapi.med.voll.api.domain.usuarios;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByUsername(String username);
     
 
 }
