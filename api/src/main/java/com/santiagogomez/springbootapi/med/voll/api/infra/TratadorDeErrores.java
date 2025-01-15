63f1845fac9f520f5ff624a761bf4c051c89fc16
Santiago Gómez <santipatito04@gmail.com>
2025-01-15 11:24:18 -0600

    Estilizar bad request
    Se diseñó una función para tratar el badrequest en caso de que falte información por añadir, y se estilizó para qué diga esécíficamente qué errores hay de una manera legible

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/TratadorDeErrores.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/infra/TratadorDeErrores.java
@@ -1,6 +1,8 @@
 package com.santiagogomez.springbootapi.med.voll.api.infra;
 
 import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.persistence.*;
@@ -13,4 +15,17 @@ public class TratadorDeErrores {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class) 
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream()
+            .map(DatosErrorValidacion::new).toList();
+
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String campo, String mensaje){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
