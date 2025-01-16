4c400f828baa551f594946851cd64cf5346bdf68
Santiago Gómez <santipatito04@gmail.com>
2025-01-16 17:02:00 -0600

    Corregir validaciones en reservas de consultas
    Se corrigió la comparación de 'm.activo' para que sea un booleano en lugar de un entero.
    - Se modificó la consulta en 'pacienteRepositorio' para verificar si el paciente está activo.
    - Se implementó un nuevo manejador de errores para 'ValidaciónException' que devuelve un 400 en caso de errores de validación.
    - Se añadió la regla de negocio para comprobar si el médico ya tiene una consulta reservada en la misma fecha y hora.

--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/paciente/PacienteRepository.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/paciente/PacienteRepository.java
@@ -3,11 +3,18 @@ package com.santiagogomez.springbootapi.med.voll.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jakarta.validation.constraints.NotNull;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByActivoTrue(Pageable paginacion);
 
+    @Query("""
+            SELECT p.activo
+            FROM Paciente p
+            WHERE
+            p.id = :idPaciente
+            """)
     boolean findActivoById(Long idPaciente);
 }
