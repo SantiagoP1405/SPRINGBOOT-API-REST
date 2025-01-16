3cfe20b8748cb71bb6732061f4125c928446de80
Santiago Gómez <santipatito04@gmail.com>
2025-01-16 14:24:26 -0600

    Agregar validación de existencia de paciente y médico
    Se implementó la validación para verificar si un paciente existe en la base de datos utilizando el método existsById.
    Se creó la excepción ValidaciónException para manejar casos donde no se encuentra un paciente o médico.
    Se implementó la lógica para elegir un médico, considerando que el ID del médico es opcional.
    Aun falta crear la lógica para el caso de elegir un id de un médico aleatorio en caso de que no se proporcione

---
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/ValidacionException.java
@@ -0,0 +1,7 @@
+package com.santiagogomez.springbootapi.med.voll.api.domain;
+
+public class ValidacionException extends RuntimeException{
+    public ValidacionException(String message) {
+        super(message);
+    }
+}
--- api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/consulta/ReservaDeConsultas.java
+++ api/src/main/java/com/santiagogomez/springbootapi/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -3,6 +3,8 @@ package com.santiagogomez.springbootapi.med.voll.api.domain.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.santiagogomez.springbootapi.med.voll.api.domain.ValidacionException;
+import com.santiagogomez.springbootapi.med.voll.api.domain.medico.Medico;
 import com.santiagogomez.springbootapi.med.voll.api.domain.medico.MedicoRepository;
 import com.santiagogomez.springbootapi.med.voll.api.domain.paciente.PacienteRepository;
 
@@ -20,10 +22,23 @@ public class ReservaDeConsultas {
     
 
     public void reservar(DatosReservaConsulta datos) {
-        var medico = medicoRepository.findById(datos.idMedico()).get();
-        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        if (!pacienteRepository.existsById(datos.idPaciente())) {
+            throw new ValidacionException("El paciente no existe con ese id");
+        }
 
-        var consulta = new Consulta(null, medico, paciente, datos.fecha());
-        consultaRepository.save(consulta);
+        if (datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())) {
+            throw new ValidacionException("El medico no existe con ese id");
+        }
+
+        var medico = elegirMedico(datos);
+                var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        
+                var consulta = new Consulta(null, medico, paciente, datos.fecha());
+                consultaRepository.save(consulta);
+    }
+        
+        
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+                System.out.println("Elegir medico");
     }
 }
